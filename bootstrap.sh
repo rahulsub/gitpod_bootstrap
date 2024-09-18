59880af26ebbd1f84e86d5a5c935e198347c4c07
Rahul Subramaniam <rahul.subramaniam@devfactory.com>
2024-09-18 12:20:43 +0000

    created bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## install tailscale
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+sudo apt-get update
+sudo apt-get install tailscale -y
+
+echo "starting tailscaled"
+nohup sudo tailscaled &> /tmp/nohup.out &
+if [ -n "$TAILSCALE_AUTHKEY" ]; then
+  sudo tailscale up --authkey="$TAILSCALE_AUTHKEY"
+fi
